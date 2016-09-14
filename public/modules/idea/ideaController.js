78b24c77168113626c0a5bbfed9db12ecbda2f53
William Yu <william.yu@xingyunzh.com>
2016-09-14 23:16:20 +0800

    idea page

--- public/modules/idea/ideaController.js
+++ public/modules/idea/ideaController.js
@@ -3,4 +3,59 @@
  */
 app.controller("ideaController", ["$scope", "$rootScope", function($scope, $rootScope){
 
+
+    $scope.ideas = data;
+
 }]);
+
+
+var data = [
+    {
+        title:"Meeting Assistant",
+        detail:"用科技的手段更好的辅助掌控会议议程, 并提供记录者、参与者工具,以更好的提高会议效率。"
+    },
+    {
+        title:"Meeting Assistant",
+        detail:"用科技的手段更好的辅助掌控会议议程, 并提供记录者、参与者工具,以更好的提高会议效率。"
+    },
+    {
+        title:"Meeting Assistant",
+        detail:"用科技的手段更好的辅助掌控会议议程, 并提供记录者、参与者工具,以更好的提高会议效率。"
+    },
+    {
+        title:"Meeting Assistant",
+        detail:"用科技的手段更好的辅助掌控会议议程, 并提供记录者、参与者工具,以更好的提高会议效率。"
+    },
+    {
+        title:"Meeting Assistant",
+        detail:"用科技的手段更好的辅助掌控会议议程, 并提供记录者、参与者工具,以更好的提高会议效率。"
+    },
+    {
+        title:"Meeting Assistant",
+        detail:"用科技的手段更好的辅助掌控会议议程, 并提供记录者、参与者工具,以更好的提高会议效率。"
+    },
+    {
+        title:"Meeting Assistant",
+        detail:"用科技的手段更好的辅助掌控会议议程, 并提供记录者、参与者工具,以更好的提高会议效率。"
+    },
+    {
+        title:"Meeting Assistant",
+        detail:"用科技的手段更好的辅助掌控会议议程, 并提供记录者、参与者工具,以更好的提高会议效率。"
+    },
+    {
+        title:"Meeting Assistant",
+        detail:"用科技的手段更好的辅助掌控会议议程, 并提供记录者、参与者工具,以更好的提高会议效率。"
+    },
+    {
+        title:"Meeting Assistant",
+        detail:"用科技的手段更好的辅助掌控会议议程, 并提供记录者、参与者工具,以更好的提高会议效率。"
+    },
+    {
+        title:"Meeting Assistant",
+        detail:"用科技的手段更好的辅助掌控会议议程, 并提供记录者、参与者工具,以更好的提高会议效率。"
+    },
+    {
+        title:"Meeting Assistant",
+        detail:"用科技的手段更好的辅助掌控会议议程, 并提供记录者、参与者工具,以更好的提高会议效率。"
+    },
+];
