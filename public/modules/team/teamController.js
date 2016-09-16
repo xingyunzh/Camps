aecc1975222b2fd967bafeab4e5d9273e9116ac8
William Yu <william.yu@xingyunzh.com>
2016-09-16 23:49:57 +0800

    team page

--- public/modules/team/teamController.js
+++ public/modules/team/teamController.js
@@ -2,5 +2,70 @@
  * Created by brillwill on 16/9/16.
  */
 app.controller("teamController", ["$scope", "$rootScope", function($scope, $rootScope){
+    var data = [
+        {
+            name:"Sun Team",
+            desc:"Focus on Java Open source technology applications"
+        },
+        {
+            name:"Earth Team",
+            desc:"Focus on MEAN stack technology applications"
+        },
+        {
+            name:"Murcury Team",
+            desc:"Focus on Internet Of Things domain innoavtions"
+        },
+        {
+            name:"Sun Team",
+            desc:"Focus on Java Open source technology applications"
+        },
+        {
+            name:"Earth Team",
+            desc:"Focus on MEAN stack technology applications"
+        },
+        {
+            name:"Murcury Team",
+            desc:"Focus on Internet Of Things domain innoavtions"
+        },
+        {
+            name:"Sun Team",
+            desc:"Focus on Java Open source technology applications"
+        },
+        {
+            name:"Earth Team",
+            desc:"Focus on MEAN stack technology applications"
+        },
+        {
+            name:"Murcury Team",
+            desc:"Focus on Internet Of Things domain innoavtions"
+        },
+        {
+            name:"Sun Team",
+            desc:"Focus on Java Open source technology applications"
+        },
+        {
+            name:"Earth Team",
+            desc:"Focus on MEAN stack technology applications"
+        },
+        {
+            name:"Murcury Team",
+            desc:"Focus on Internet Of Things domain innoavtions"
+        },
+        {
+            name:"Sun Team",
+            desc:"Focus on Java Open source technology applications"
+        },
+        {
+            name:"Earth Team",
+            desc:"Focus on MEAN stack technology applications"
+        },
+        {
+            name:"Murcury Team",
+            desc:"Focus on Internet Of Things domain innoavtions"
+        },
 
+    ];
+
+    $scope.teams = data;
 }]);
+
