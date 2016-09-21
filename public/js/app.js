3bb1a2ac53af5b1ec9d3905a225bdab0c103d162
William Yu <william.yu@xingyunzh.com>
2016-09-22 01:04:15 +0800

    add toaster

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,7 @@
 /**
  * Created by brillwill on 16/9/14.
  */
-var app = angular.module("app", ["ui.router", "ui.bootstrap"])
+var app = angular.module("app", ["ui.router", "ui.bootstrap", "toaster", "ngAnimate"])
 .run(["$rootScope", "$state", "$stateParams", function($rootScope, $state, $stateParams){
     $rootScope.$state = $state;
     $rootScope.$stateParams = $stateParams;
--- public/modules/asset/assetController.js
+++ public/modules/asset/assetController.js
@@ -1,7 +1,8 @@
 /**
  * Created by brillwill on 16/9/14.
  */
-app.controller("assetController", ["$scope", "$rootScope", "httpHelper", function($scope, $rootScope, httpHelper){
+app.controller("assetController", ["$scope", "$rootScope", "httpHelper", "util", "toaster",
+    function($scope, $rootScope, httpHelper, util, toaster){
     $scope.items = [];
     $scope.isError = false;
 
@@ -13,6 +14,35 @@ app.controller("assetController", ["$scope", "$rootScope", "httpHelper", functio
             $scope.isError = true;
             $scope.items = [];
         });
+    }
+    
+    $scope.onTest = function () {
+        util.confirmationStep("Test Title", "Are you ready for Campro?").then(function success() {
+            $scope.result="Yes, I'm ready for Campro.";
+        }, function failure(){
+            $scope.result = "No, I'm not."
+        })
+    }
+
+    $scope.onModalInputTest = function(){
+        util.modalTextInputStep("Input your idea","Placeholder").then(function ok(data) {
+            $scope.inputText = data;
+        }, function cancel(data) {
+            $scope.inputText = "";
+        })
+    }
+    
+    $scope.toasterPop = function () {
+        toaster.pop({
+            type:'success',
+            title:'title',
+            body:'bodyTextContent',
+            timeout:0
+        });
+    }
+    
+    $scope.toasterClear = function () {
+        toaster.clear();
     }
 
 }]);
