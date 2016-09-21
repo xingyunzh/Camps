3e18d545f97e80ab3550688181712e8489c4e1e7
William Yu <william.yu@xingyunzh.com>
2016-09-21 15:01:33 +0800

    http helper

--- public/modules/asset/assetController.js
+++ public/modules/asset/assetController.js
@@ -1,6 +1,18 @@
 /**
  * Created by brillwill on 16/9/14.
  */
-app.controller("assetController", ["$scope", "$rootScope", function($scope, $rootScope){
-    
+app.controller("assetController", ["$scope", "$rootScope", "httpHelper", function($scope, $rootScope, httpHelper){
+    $scope.items = [];
+    $scope.isError = false;
+
+    $scope.onRefresh = function () {
+        httpHelper.sendRequest("GET", "/demo").then(function success(data) {
+            $scope.items = data;
+            $scope.isError = false;
+        },function fail(){
+            $scope.isError = true;
+            $scope.items = [];
+        });
+    }
+
 }]);
