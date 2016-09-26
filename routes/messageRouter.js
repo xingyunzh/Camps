0330280485f32eb5f08326d5ceab29f68f4ed1e6
Morris Lew <morrislew@outlook.com>
2016-09-26 23:53:52 +0800

    私信功能原型及测试

--- routes/messageRouter.js
+++ routes/messageRouter.js
@@ -2,7 +2,7 @@ var messageRouter = require("express").Router();
 
 var messageController = require('../controllers/messageController');
 
-messageRouter.get('/getting', messageController.getMessage);
+messageRouter.post('/getting', messageController.getMessage);
 messageRouter.post('/sending', messageController.sendMessage);
 
 module.exports = messageRouter;
