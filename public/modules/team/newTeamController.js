96e89e8ccadb4ab8a191cb91f4c4192cbfeb5c72
William Yu <william.yu@xingyunzh.com>
2016-09-29 08:35:25 +0800

    detail editing

--- public/modules/team/newTeamController.js
+++ public/modules/team/newTeamController.js
@@ -1,7 +1,15 @@
 /**
  * Created by brillwill on 16/9/28.
  */
-app.controller("newTeamController", ["$scope", "$rootScope", function ($scope, $rootScope) {
+app.controller("newTeamController", ["$scope", "$rootScope", "util", function ($scope, $rootScope, util) {
     $scope.form = {};
 
+    $scope.onSubmitButton = function () {
+        util.confirmationStep("新建","你在同一时刻只能加入一个团队,是否确认自己新建一个团队?")
+            .then(function ok() {
+                $rootScope.$state.go("nav.team");
+            }, function cancel() {
+
+            })
+    }
 }]);
--- public/modules/team/teamDetailController.js
+++ public/modules/team/teamDetailController.js
@@ -1,7 +1,27 @@
 /**
  * Created by brillwill on 16/9/28.
  */
-app.controller("teamDetailController", ["$scope", "$rootScope", function ($scope, $rootScope) {
-    $scope.form = {};
+app.controller("teamDetailController", ["$scope", "$rootScope", "util", function ($scope, $rootScope, util) {
+    $scope.form = {
+        name:$rootScope.theTeam.name,
+        description:$rootScope.theTeam.description,
+        leader:$rootScope.theTeam.leader,
+        members:$rootScope.theTeam.members
+    };
+
+    $scope.isEditing = false;
+    $scope.onEditButton = function () {
+        if ($scope.isEditing){
+            util.confirmationStep("提交修改", "是否保存并提交您的修改?")
+                .then(function ok() {
+                    $scope.isEditing = false;
+                }, function cancel() {
+
+                });
+        }
+        else {
+            $scope.isEditing = true;
+        }
+    }
 
 }]);
