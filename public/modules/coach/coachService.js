55163f79764f2b73b97b79ca7dfda3e33527573c
William Yu <william.yu>
2016-11-18 08:11:48 +0800

    user idea

--- public/modules/coach/coachService.js
+++ public/modules/coach/coachService.js
@@ -2,9 +2,12 @@
  * Created by brillwill on 16/10/1.
  */
 app.service("coachService", ['httpHelper', 'util', '$q', function (httpHelper, util,$q) {
-    this.coachSource = function () {
-        return util.promiseWithResolve(_.filter(userData, function (item) {
-            return _.includes(item.roles, "coach");
-        }));
+    this.coachSource = function (keyword) {
+        return httpHelper.sendRequest('POST', './api/user/list', {
+            role:"Coach",
+            keyword:keyword
+        }).then(function(data){
+            return data.users.list;
+        });
     };
 }]);
--- public/modules/profile/profileService.js
+++ public/modules/profile/profileService.js
@@ -1,19 +1,4 @@
 app.service("profileService", ['httpHelper', function (httpHelper) {
-    this.loginByWechat = function (code,callback) {
-        httpHelper.sendRequest("POST", "./public/login/wechat",{
-			code:code
-		}).then(function success(data) {
-			if (data.user != null) {
-				callback(null,data);
-			} else {
-				callback(new Error('Invalid Credentials'));
-			}
-            
-        },function fail(err){
-            callback(err);
-        });
-    };
-
     this.getProfile = function(callback) {
     	httpHelper.sendRequest("POST", "./api/user/profile",{
 			code:code
