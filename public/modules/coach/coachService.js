ab4277c96a71d166aaaee17abf0867a48ab33413
William Yu <william.yu>
2016-12-05 16:11:30 +0800

    team detail UI

--- public/modules/coach/coachService.js
+++ public/modules/coach/coachService.js
@@ -3,11 +3,8 @@
  */
 app.service("coachService", ['httpHelper', 'util', '$q', function (httpHelper, util,$q) {
     this.coachSource = function (keyword) {
-        return httpHelper.sendRequest('POST', './api/user/list', {
-            role:"Coach",
-            keyword:keyword
-        }).then(function(data){
-            return data.users.list;
+        return httpHelper.sendRequest('GET', './api/user/list?roles=coach&keyword='+keyword).then(function(data){
+            return data.users.users;
         });
     };
 }]);
