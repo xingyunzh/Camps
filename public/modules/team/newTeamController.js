dc1fe3691879f4b4695539109342851664c226c8
William Yu <william.yu>
2016-12-06 17:16:31 +0800

    team page service

--- public/modules/team/newTeamController.js
+++ public/modules/team/newTeamController.js
@@ -1,28 +1,26 @@
 /**
  * Created by brillwill on 16/9/28.
  */
-app.controller("newTeamController", ["$scope", "$rootScope", "util", function ($scope, $rootScope, util) {
+app.controller("newTeamController", ["$scope", "$rootScope", "teamService", "util", function ($scope, $rootScope, teamService, util) {
     $scope.form = {
         name: null,
-        description: null,
-        leader: null,
-        members: [],
-        teamId: "20160701001",
-        isActive: true,
-        birthday: new Date("2015-12-12"),
-        project: null,
-        deactiveDate: null,
-        activeDate: new Date("2016-07-01"),
+        background: null,
+        lead: $rootScope.currentUser,
+        member: [],
+        coach:null
     };
 
     $scope.onSubmitButton = function () {
         util.confirmationStep("新建","你在同一时刻只能加入一个团队,是否确认自己新建一个团队?")
-            .then(function ok() {
-                $rootScope.theTeam = $scope.form;
-
-                $rootScope.$state.go("nav.team-detail");
-            }, function cancel() {
-
+            .then(function () {
+                return teamService.add($scope.form);
             })
-    }
+            .then (function (data) {
+                $rootScope.theTeam = data.team;
+                $rootScope.$state.go('nav.team-detail');
+            })
+            .catch(function (error){
+                console.log("Creation error:"+error.toString());
+            });
+    };
 }]);
