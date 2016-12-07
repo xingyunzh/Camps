b2478e192830476b7e16cd46a274d17ebccbfbaf
William Yu <william.yu>
2016-12-07 14:16:31 +0800

    project

--- public/modules/project/projectController.js
+++ public/modules/project/projectController.js
@@ -1,8 +1,11 @@
 /**
  * Created by brillwill on 16/9/14.
  */
-app.controller("projectController", ["$scope", "$rootScope", function($scope, $rootScope){
-    $scope.projects = projectData;
+app.controller("projectController", ["$scope", "$rootScope","projectService", function($scope, $rootScope, projectService){
+    projectService.projectSource().then(function(data){
+        console.log(JSON.stringify(data));
+        $scope.projects = data.projects;
+    })
 
     $scope.handleProjectClicked = function (project) {
         $rootScope.theProject = project;
